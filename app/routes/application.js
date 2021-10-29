979cbe907dff42acea14e1ab3e1b85289c61713c
allthesignals <>
2021-10-29 16:51:51 -0500

    New pin; add logos

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,7 +1,8 @@
 import Route from '@ember/routing/route';
 import { csv } from 'd3-fetch';
 
-const GOOGLE_SHEET_URL="https://docs.google.com/spreadsheets/d/e/2PACX-1vS_1zhA83J62qN3aVpKKTYmmXSdJ6AWWqXAlRC7T-eUtwBnbS_4RhugdIH5kz3Gw8NN8ZBkH5EnZJZO/pub?gid=0&single=true&output=csv";
+const GOOGLE_SHEET_URL =
+  'https://docs.google.com/spreadsheets/d/e/2PACX-1vS_1zhA83J62qN3aVpKKTYmmXSdJ6AWWqXAlRC7T-eUtwBnbS_4RhugdIH5kz3Gw8NN8ZBkH5EnZJZO/pub?gid=0&single=true&output=csv';
 
 export default class ApplicationRoute extends Route {
   model() {
--- config/environment.js
+++ config/environment.js
@@ -23,14 +23,15 @@ module.exports = function (environment) {
     },
 
     'mapbox-gl': {
-      accessToken: 'pk.eyJ1Ijoid21hdHRnYXJkbmVyIiwiYSI6ImNrdmJvZHlobmFwZHEycG1hMndtNHd0Y2MifQ.JWKGif2DQFgXI4EAvzEVTA',
+      accessToken:
+        'pk.eyJ1Ijoid21hdHRnYXJkbmVyIiwiYSI6ImNrdmJvZHlobmFwZHEycG1hMndtNHd0Y2MifQ.JWKGif2DQFgXI4EAvzEVTA',
       map: {
         style: 'mapbox://styles/wmattgardner/ckvbqf6q25e5a15tbgp3qjmva',
         zoom: 13,
-        center: [ -95.98378, 36.14139 ],
+        center: [-95.98378, 36.14139],
       },
       marker: {
-        offset: [ -1, -1 ]
+        offset: [-1, -1],
       },
     },
   };
