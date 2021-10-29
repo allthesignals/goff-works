10db80317a1184f82eabbc0b5a4f8045c9b8d9b7
allthesignals <>
2021-10-29 01:04:56 -0500

    Fingerprinting off

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
-    enabled: false,
+    fingerprint: {
+      enabled: false,
+    },
     // Add options here
   });
 
